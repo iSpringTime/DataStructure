95b632d50c30b3b323ffe31ca6f3e8e7580246b1
zhaocl <45536703+iSpringTime@users.noreply.github.com>
2020-01-13 19:53:58 +0800

    Add files via upload

---
+++ common_dataStruct.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Copyright (c) JingMingGroup All rights reserved.
+-------------------------------------------------------------------------------
+  File name     : queue.h
+  Author        : iSpringTime
+  Create        : 2019.10.23
+  Description   : queue operation (demo)
+******************************************************************************/
+
+#ifndef _COMMON_DATASTRUCT_H_
+#define _COMMON_DATASTRUCT_H_
+
+int str2Num(char *str);
+
+#endif
