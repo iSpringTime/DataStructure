c2decc0f237526dce0f63b0b81be4ab68b76ecdb
zhaocl <45536703+iSpringTime@users.noreply.github.com>
2020-01-13 19:51:32 +0800

    Update queue_operation.c

--- src/queue_operation.c
+++ src/queue_operation.c
@@ -1,53 +1,63 @@
-/******************************************************************************
- * Copyright (c) JingMingGroup All rights reserved.
--------------------------------------------------------------------------------
-  File name     : queue_operation.c
-  Author        : iSpringTime
-  Create        : 2019.10.23
-  Description   : queue_operation (demo)
-******************************************************************************/
-
 #include "queue.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "malloc.h"
+#include "math.h"
 
-#ifdef __cplusplus
-#if __cplusplus
-extern "C" {
-#endif
-#endif /* __cplusplus */
-
-status EnQueue(QueueList *queueList, QElemType e)
+//初始化
+//开始必然是空队列，队尾指针和队头指针都指向头结点
+void initQueue(LinkQueue *queue)
 {
-  QueuePtr node = NULL;
-  node = (QueuePtr)malloc(sizeof(QueueNode));
-
-  node->date = e;
-  node->next = NULL;
-  queueList->rear->next = node;
-  queueList->rear = node;
-  return OK;
+    //初始化头结点
+    queue->front = queue->rear = (Queue)malloc(sizeof(QNode));
+    
+    if (NULL == queue->front) {
+        exit(0);
+    }
+    
+    queue->front->next = NULL;
 }
 
-status DeQueue(QueueList *queueList, QElemType *e)
+//判空
+bool isEmpty(LinkQueue queue)
 {
-  QueuePtr node = NULL;
-
-  if (queueList->rear == queueList->front) {
-    return ERR;
-  }
-
-  node = queueList->front->next;
-
-  *e = node->date;
-  queueList->front->next = node->next;
-
-  if(node == queueList->rear)
-    queueList->front = queueList->rear;
-  free(node);
-  return OK;
+    return queue.rear == queue.front ? true : false;
 }
 
-#ifdef __cplusplus
-#if __cplusplus
+//入队,只在一端入队，另一端出队，同样入队不需要判满
+void insertQueue(LinkQueue *queue, QElemType temp)
+{
+    Queue q = (Queue)malloc(sizeof(QNode));
+    
+    if (NULL == q) {
+        exit(0);
+    }
+
+    //插入数据
+    q->data = temp;
+    q->next = NULL;
+
+    //rear 总是指向队尾元素
+    queue->rear->next = q;
+    queue->rear = q;
 }
-#endif
-#endif /* __cplusplus */
+
+//出队，需要判空
+void deleteQueue(LinkQueue *queue, QElemType *ret)
+{
+    Queue q = NULL;
+
+    if (!isEmpty(*queue)) {
+        q = queue->front->next;
+        *ret = q->data;
+        queue->front->next = q->next;
+        
+        //这句很关键，不能丢
+        if (queue->rear == q) {
+            queue->rear = queue->front;
+        }
+        
+        free(q);
+    }
+}
